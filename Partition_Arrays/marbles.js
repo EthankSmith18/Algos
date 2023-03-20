1e4d63ded251f0e323ac2aafc55a58104052b9dd
Ethan Smith <EthanKSmith@gmail.com>
2023-03-20 14:57:22 -0700

    Completed_Partitioning

--- Partition_Arrays/marbles.js
+++ Partition_Arrays/marbles.js
@@ -5,3 +5,48 @@
 // 1 - White
 // 2 - Blue
 
+let array1 = [0,1,1,2,1,0,0,2,1]
+let array2 = [1,1,0]
+let array3 = [2,2,2]
+
+let pivot = 1;
+
+function marbleSort(array){
+  if( array == null){
+    return
+  }
+
+  let low_boundary = 0;
+  let high_boundary = array.length -1;
+  let temp;
+
+  let i = 0;
+  while( i <= high_boundary){
+    if(array[i] < pivot){
+      temp = array[low_boundary];
+      array[low_boundary] = array[i];
+      array[i] = temp;
+      i ++;
+      low_boundary ++;
+    }
+    else if(array[i] > pivot){
+      temp = array[high_boundary];
+      array[high_boundary] = array[i];
+      array[i] = temp;
+      high_boundary --;
+    }
+    else if(array[i] == pivot){
+      i++;
+    }
+    else{
+      throw new Error('Unknown Color')
+    }
+
+
+  }
+  return array;
+}
+
+console.log(marbleSort(array1))
+console.log(marbleSort(array2))
+console.log(marbleSort(array3))
