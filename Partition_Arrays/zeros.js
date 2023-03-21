12532aa5c7d095f18dcdd934dda02d9d1247af66
Ethan Smith <EthanKSmith@gmail.com>
2023-03-21 13:00:22 -0700

    Partitioning Practice

--- Partition_Arrays/zeros.js
+++ Partition_Arrays/zeros.js
@@ -4,37 +4,70 @@
 
 let array1 = [4, 2, 0, 1, 0, 3, 0];
 
-function moveZeroesFront(array) {
+// function moveZeroesFront(array) {
+//   let temp;
+//   let boundary = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] == 0) {
+//       temp = array[boundary];
+//       array[boundary] = array[i];
+//       array[i]=temp;
+//       console.log(array[i])
+//       boundary += 1;
+//     }
+//   }
+
+//   return array;
+// }
+
+// console.log(moveZeroesFront(array1))
+
+// function moveZeroesBack(array) {
+//   let temp;
+//   let boundary = array.length-1;
+//   for (let i = array.length-1; i >= 0; i--) {
+//     if (array[i] == 0) {
+//       temp = array[boundary];
+//       array[boundary] = array[i];
+//       array[i]=temp;
+//       boundary -= 1;
+//     }
+//   }
+
+//   return array;
+// }
+
+// console.log(moveZeroesBack(array1))
+
+
+function moveZeroesFront(array){
   let temp;
   let boundary = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] == 0) {
+  for(let i = 0; i < array.length; i++){
+    if(array[i] == 0){
       temp = array[boundary];
       array[boundary] = array[i];
-      array[i]=temp;
-      console.log(array[i])
-      boundary += 1;
+      array[i] = temp;
+      boundary ++;
     }
   }
-
   return array;
 }
 
 console.log(moveZeroesFront(array1))
 
-function moveZeroesBack(array) {
+function moveZeroesBack(array){
   let temp;
-  let boundary = array.length-1;
-  for (let i = array.length-1; i >= 0; i--) {
-    if (array[i] == 0) {
+  let boundary = array.length -1;
+  for(let i = array.length -1; i >= 0; i--){
+    if(array[i] == 0){
       temp = array[boundary];
       array[boundary] = array[i];
-      array[i]=temp;
-      boundary -= 1;
+      array[i] = temp;
+      boundary--;
     }
   }
-
-  return array;
+  return array
 }
 
 console.log(moveZeroesBack(array1))
