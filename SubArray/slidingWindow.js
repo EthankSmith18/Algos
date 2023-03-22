091574d8d2fc95253398cec739c7009fe7bdd6f7
Ethan Smith <EthanKSmith@gmail.com>
2023-03-22 12:30:17 -0700

    slidingWindow Practice

--- SubArray/slidingWindow.js
+++ SubArray/slidingWindow.js
@@ -31,9 +31,79 @@
 //       return ([start, end])
 //     }
 //   }
+  // return null;
+// }
+
+// console.log(slidingWindow(array1))
+
+
+
+// function slidingWindow(array){
+//   if(array == null || array.length == 0){
+//     return null;
+//   }
+//   let start = 0;
+//   let end = 0;
+//   let sum = array[0]
+
+//   while(end < array.length){
+//     if(start > end){
+//       end = start;
+//       sum = array[start];
+//     }
+//     else if(sum < target){
+//       if(end == array.length -1){
+//         break;
+//       }
+//       end++;
+//       sum += array[end];
+//     }
+//     else if(sum > target){
+//       sum -= array[start];
+//       start++;
+//     }
+//     else{
+//       return([start, end]);
+//     }
+//   }
+
+//   return null;
+
 // }
 
 // console.log(slidingWindow(array1))
 
 let array1 = [1, 2, 3, 5, 2];
 let target = 8;
+
+function slidingWindow(array){
+  if(array == null || array.length == 0){
+    return null;
+  }
+  let start = 0;
+  let end = 0;
+  let sum = array[0];
+  while(end < array.length){
+    if(start > end){
+      end = start;
+      sum = array[start];
+    }
+    else if(sum < target){
+      if(end == array.length-1){
+        break;
+      }
+      end++;
+      sum += array[end];
+    }
+    else if(sum > target){
+      sum -= array[start];
+      start++;
+    }
+    else{
+      return ([start, end])
+    }
+  }
+  return null;
+}
+
+console.log(slidingWindow(array1))
